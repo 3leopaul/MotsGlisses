ff9df60d9fe3206395443f92c6e429bc6d092f3d
3leopaul <leo-paul.chauvigne@edu.devinci.fr>
2023-11-28 16:59:16 +0100

    Creer mais pas écrit methode toString

---
+++ Dictionnaire.cs
@@ -0,0 +1,16 @@
+using System.Security.Cryptography.X509Certificates;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dictionnaire
+{
+    public class Dictionnaire
+    {
+        public string toString(){
+            
+
+        }
+    }
+}
