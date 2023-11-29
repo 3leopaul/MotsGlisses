7d05e993f0b46edaa039983f4e1b7b3bf7ff3b0d
3leopaul <leo-paul.chauvigne@edu.devinci.fr>
2023-11-29 09:30:21 +0100

    Ajout RechDicoRecursif(Pas finie) et attributs temporaires

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -1,16 +1,65 @@
+using System.Runtime.CompilerServices;
+using System.Globalization;
 using System.Security.Cryptography.X509Certificates;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace dictionnaire
+namespace MotsGlisses
 {
     public class Dictionnaire
     {
-        public string toString(){
-            
+        string nomTemp;
+        string[] dicTemp;
 
+        public Dictionnaire(string n, string[] d){
+            this.nomTemp=n;
+            this.dicTemp=d;
         }
+
+        public string NomTemp(){
+            get{return nomTemp;}
+        }
+
+        public string[] DicTemp{
+            get{return dicTemp;}
+        }
+
+
+        // Methode recherche RechDichoRecursif(Pas validée encore) :
+        static bool RechDichoRecursif(int debut, int fin, string[] dico, string mot)
+        {
+            if(fin<debut || dico==null || dico.Length==0|| String.Compare(dico[fin],mot)==-1 || String.Compare(mot,dico[debut])==-1){
+                return false;
+            }if(debut<=fin){
+                int c =(debut+fin)/2;
+                if (dico[c]==mot){
+                    return true;
+                }if(String.Compare(dico[c], mot)==-1){
+                    debut=c+1;
+                    return RechDichoRecursif(debut, fin, dico, mot);
+                }if(debut==fin){
+                    if(dico[c]==mot){
+                        return true;
+                    }else{
+                        return false;
+                    }
+                }else{
+                    fin=c-1;
+                    return RechDichoRecursif(debut, fin, dico, mot);
+                }
+            }else{
+                return false;
+            }
+    
+        }
+
+        //Fin methode DicoRec
+
+        //Methode tri dico
+        
+
+        
     }
 }
--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using System.Collections.Generic;
+using Internal;
+using System;
 
 namespace MotsGlisses
 {
@@ -7,6 +9,8 @@ namespace MotsGlisses
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Dictionaire dicoT = new Dictionnaire("Dico1", {"Aaa","Aab","Aba","Caa","Zo"});
+            Console.WriteLine(RechDichoRecursif(0,4,dicoT.DicoTemp,"Aba"));
         }
     }
 }
