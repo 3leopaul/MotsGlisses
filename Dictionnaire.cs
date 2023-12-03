c996bcbc90659509eb75bbb8521e1964c2729298
3leopaul <leo-paul.chauvigne@edu.devinci.fr>
2023-12-03 18:52:26 +0100

    Finition de la classe dico ENFINNN

--- Dictionnaire.cs
+++ Dictionnaire.cs
@@ -10,27 +10,74 @@ namespace MotsGlisses
 {
     public class Dictionnaire
     {
-        string nomTemp;
-        string[] dicTemp;
+        string langue;
+        string[][] dicoTab = new string[26][];
+        string nomFichier;        
+        public Dictionnaire(string nomDuFichier, string l){
+            this.nomFichier=nomDuFichier;
+            this.langue=l;
 
-        public Dictionnaire(string n, string[] d){
-            this.nomTemp=n;
-            this.dicTemp=d;
+            StreamReader sr = new StreamReader(nomDuFichier);
+            try{
+                string s;
+                int i=0;
+                s=sr.ReadLine();
+                while(i<26){
+                    if(s!=null){
+                        this.dicoTab[i]=s.Split(' ');
+                        TriFusionRecursif(this.dicoTab[i], 0, this.dicoTab[i].Length-1);
+                    }else{
+                        this.dicoTab[i]=null;
+                    }
+                    s=sr.ReadLine();
+                    i++;
+                }
+                sr.Close();
+            }
+            catch (FileNotFoundException)
+            {
+                Console.WriteLine($"Fichier introuvable: {nomDuFichier}");
+            }
+            catch (IndexOutOfRangeException)
+            {
+                Console.WriteLine("Erreur: IndexOutOfRange");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Erreur convertir en double");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Erreur non pévue: {ex.Message}");
+            }
+
+
         }
 
-        public string NomTemp{
+        public string NomFichier{
             get
             {
-                return this.nomTemp;
+                return this.nomFichier;
             }
         }
 
-        public string[] DicTemp{
-            get
-            {
-                return this.dicTemp;
+
+        // Methode pour decrire une instance de la classe Dictionaire
+        public string DicoToString(){
+            int dicoTabILongueur=0;
+            string s="Langue: "+langue;
+            char[] alphabet = {'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+            for(int i=0; i<26; i++){
+                if(dicoTab[i]!=null){
+                    dicoTabILongueur=dicoTab[i].Length-1;
+                }
+                s=s+"\n"+alphabet[i]+": "+dicoTabILongueur+" Mots";
+                dicoTabILongueur=0;
             }
+            return s;
         }
+
+        //fin methode DicoToString
 
 
         // Methode recherche RechDichoRecursif(Pas validée encore) :
@@ -62,7 +109,66 @@ namespace MotsGlisses
         }
 
         //Fin methode DicoRec
+        public static void TriFusionRecursif(string[] tab, int debut, int fin)
+        {
+            if (debut < fin)
+            {
+                int m = debut + (fin - debut) / 2;
 
+                TriFusionRecursif(tab, debut, m);
+                TriFusionRecursif(tab, m + 1, fin);
+                Fusion(tab, debut, m, fin);
+            }
+        }
+
+        private static void Fusion(string[] tab, int debut, int milieu, int fin)
+        {
+            int i, j, k;
+            int n1 = milieu - debut + 1;
+            int n2 = fin - milieu;
+            string[] L = new string[n1];
+            string[] R = new string[n2];
+
+            for (i = 0; i < n1; i++)
+                L[i] = tab[debut + i];
+
+            for (j = 0; j < n2; j++)
+                R[j] = tab[milieu + 1 + j];
+
+            i = 0;
+            j = 0;
+            k = debut;
+
+            while (i < n1 && j < n2)
+            {
+                if (String.Compare(L[i], R[j]) < 1)
+                {
+                    tab[k] = L[i];
+                    i++;
+                }
+                else
+                {
+                    tab[k] = R[j];
+                    j++;
+                }
+
+                k++;
+            }
+
+            while (i < n1)
+            {
+                tab[k] = L[i];
+                i++;
+                k++;
+            }
+
+            while (j < n2)
+            {
+                tab[k] = R[j];
+                j++;
+                k++;
+            }
+        }
         //Methode tri dico
         
 
--- Program.cs
+++ Program.cs
@@ -7,9 +7,9 @@ namespace MotsGlisses
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            string[] st = {"Aaa","Aab","Aba","Caa","Zo"};
-            Dictionnaire dicoT = new Dictionnaire("Dico1", st);
-            Console.WriteLine(Dictionnaire.RechDichoRecursif(0,4,dicoT.DicTemp,"Aba"));
+            string nomFichier = "Mots_Français.txt";
+            Dictionnaire dico = new Dictionnaire(nomFichier, "Francais");
+            Console.Write(dico.DicoToString());
         }
     }
 }
